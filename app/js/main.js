2aa4ddd661eba0c704d0fe2cd022569a602fd899
waxa <waxaman@gmail.com>
2015-10-15 01:11:17 +0200

    fix audio routes

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,18 @@
 var playList = [
 	{
 		name: '01',
-		src_mp3: '../audios/1.mp3',
-		src_ogg: '../audios/1.ogg'
+		src_mp3: 'audios/1.mp3',
+		src_ogg: 'audios/1.ogg'
 	},
 	{
 		name: '02',
-		src_mp3: '../audios/2.mp3',
-		src_ogg: '../audios/2.ogg'
+		src_mp3: 'audios/2.mp3',
+		src_ogg: 'audios/2.ogg'
 	},
 	{
 		name: '03',
-		src_mp3: '../audios/3.mp3',
-		src_ogg: '../audios/3.ogg'
+		src_mp3: 'audios/3.mp3',
+		src_ogg: 'audios/3.ogg'
 	}
 ];
 
