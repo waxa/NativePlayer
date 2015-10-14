fb0ccfd5ce44029cae53d78533f95f1f9ae1b8b5
waxa <waxaman@gmail.com>
2015-10-15 00:51:06 +0200

    ajustada la interfaz a la que tiene mamamia, defectos propios de la falta del javascript con el que se inicializa la página

--- app/js/main.js
+++ app/js/main.js
@@ -13,57 +13,7 @@ var playList = [
 		name: '03',
 		src_mp3: '../audios/3.mp3',
 		src_ogg: '../audios/3.ogg'
-	},
-	{
-		name: '04',
-		src_mp3: '../audios/4.mp3',
-		src_ogg: '../audios/4.ogg'
-	},
-	{
-		name: '05',
-		src_mp3: '../audios/5.mp3',
-		src_ogg: '../audios/5.ogg'
-	},
-	{
-		name: '06',
-		src_mp3: '../audios/6.mp3',
-		src_ogg: '../audios/6.ogg'
-	},
-	{
-		name: '07',
-		src_mp3: '../audios/7.mp3',
-		src_ogg: '../audios/7.ogg'
-	},
-	{
-		name: '08',
-		src_mp3: '../audios/8.mp3',
-		src_ogg: '../audios/8.ogg'
-	},
-	{
-		name: '09',
-		src_mp3: '../audios/9.mp3',
-		src_ogg: '../audios/9.ogg'
-	},
-	{
-		name: '10',
-		src_mp3: '../audios/10.mp3',
-		src_ogg: '../audios/10.ogg'
-	},
-	{
-		name: '11',
-		src_mp3: '../audios/11.mp3',
-		src_ogg: '../audios/11.ogg'
-	},
-	{
-		name: '12',
-		src_mp3: '../audios/12.mp3',
-		src_ogg: '../audios/12.ogg'
-	},
-	{
-		name: '13',
-		src_mp3: '../audios/13.mp3',
-		src_ogg: '../audios/13.ogg'
-	},
+	}
 ];
 
 
--- app/js/player.js
+++ app/js/player.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function NativePlayer ( playList ) {
 	var np = this;
 
@@ -5,58 +7,154 @@ function NativePlayer ( playList ) {
 	this.indice = 0;
 
 	this.player = document.getElementById('player');
-	this.source_mp3 = document.createElement('source');
-	this.source_ogg = document.createElement('source');
+	this.songtitle = document.getElementById('songtitle');
 
-	this.source_ogg.setAttribute('src', playList[this.indice].src_ogg);
+	this.source_ogg = document.createElement('source');
+	this.source_mp3 = document.createElement('source');
+
 	this.source_ogg.setAttribute('type', 'audio/ogg');
-	this.source_mp3.setAttribute('src', playList[this.indice].src_mp3);
 	this.source_mp3.setAttribute('type', 'audio/mp3');
 
 	this.player.appendChild(this.source_mp3);
 	this.player.appendChild(this.source_ogg);
 
+
+
 	this.onPlayClick = function () {
 		np.player.play();
+		np.playButton.style.display = 'none';
+		np.pauseButton.style.display = 'inline-block';
 	};
 
 	this.onPauseClick = function () {
-		np.player.pause();	
+		np.player.pause();
+		np.pauseButton.style.display = 'none';
+		np.playButton.style.display = 'inline-block';
 	};
 
-	this.onNextClick = function () {
-		np.indice += 1;
-		if (np.indice >= np.playList.length) 
-			np.indice = 0;
-		np.setNewSong();
+	this.onStopClick = function () {
+		np.player.pause();
+		np.player.load();
+		np.pauseButton.style.display = 'none';
+		np.playButton.style.display = 'inline-block';
 	};
 
 	this.onPrevClick = function () {
 		np.indice -= 1;
 		if (np.indice < 0) 
 			np.indice = np.playList.length - 1;
-		np.setNewSong();
+		np.loadNewSong();
 	};
 
-	this.setNewSong = function () {
+	this.onNextClick = function () {
+		np.indice += 1;
+		if (np.indice >= np.playList.length) 
+			np.indice = 0;
+		np.loadNewSong();
+	};
+
+	this.loadNewSong = function () {
 		np.player.pause()
-		this.source_ogg.setAttribute('src', playList[np.indice].src_ogg);
-		this.source_mp3.setAttribute('src', playList[np.indice].src_mp3);
+		this.source_ogg.setAttribute('src', np.playList[np.indice].src_ogg);
+		this.source_mp3.setAttribute('src', np.playList[np.indice].src_mp3);
 		np.player.load();
-		np.player.oncanplaythrough = np.player.play();
+		np.songtitle.innerHTML = np.playList[np.indice].name;
+		np.player.oncanplay = np.onPlayClick();
 	};
 
-	this.playButton = document.getElementById('play');
+	this.onProgress = function () {
+		var position = np.player.currentTime;
+		position = position / np.player.duration;
+		position = position * 100;
+		np.progressBar.style.width = (position + '%');
+	}
+
+	this.onProgressClick = function (e) {
+		console.log("click");
+		var percent = e.pageX - jQuery('#playprogress > div').offset().left;
+		percent = (percent / jQuery('#playprogress > div').width()) * 100;
+		if ( percent > 100 ) percent = 100;
+		if ( percent < 0 ) percent = 0;
+		np.progressBar.style.width = (percent + '%');
+		var position = (np.player.duration / 100) * percent;
+		np.player.currentTime = position;
+	};
+
+	this.onListClick = function () {
+		if (np.songList.style.display == 'none'){
+			np.songList.style.display = 'block';
+		} else {
+			np.songList.style.display = 'none';
+		}
+	};
+
+	this.createSongList = function () {
+		var songInnerList = "";
+		np.playList.forEach(function(element, index, array) {
+			songInnerList += '<li>' + element.name + '</li>';
+		});
+		return songInnerList;
+	};
+
+	this.setUpSongList = function () {
+		jQuery('#songlist ul').children().each(function (index, element) {
+			var indice2 = index;
+			jQuery(this).on('click', function () {
+				np.indice = indice2;
+				np.loadNewSong();
+				np.onListClick();
+			})
+		});
+	};
+
+	this.onVolumeChange = function (newVol) {
+		np.player.volume = newVol;
+	};
+
+	this.onMuteClick = function () {
+		if (np.volume.classList.contains('mute')){
+			np.volume.classList.remove('mute');
+			np.player.muted = false;
+		} else {
+			np.volume.classList.add('mute');
+			np.player.muted = true;
+		}
+	};
+
+	this.songList = document.getElementById('songlist');
+	this.songList.style.display = 'none';
+	this.listButton = document.getElementById('playlist');
+	this.listButton.addEventListener('click', this.onListClick, false);
+
+	this.playButton = document.getElementById('playsong');
 	this.playButton.addEventListener('click', this.onPlayClick, false);
 
-	this.stopButton = document.getElementById('stop');
-	this.stopButton.addEventListener('click', this.onPauseClick, false);
+	this.pauseButton = document.getElementById('pausesong');
+	this.pauseButton.addEventListener('click', this.onPauseClick, false);
 
-	this.nextButton = document.getElementById('forward');
-	this.nextButton.addEventListener('click', this.onNextClick, false);
+	this.stopButton = document.getElementById('stopsong');
+	this.stopButton.addEventListener('click', this.onStopClick, false);
 
-	this.prevButton = document.getElementById('backward');
+	this.prevButton = document.getElementById('prevsong');
 	this.prevButton.addEventListener('click', this.onPrevClick, false);
 
-	console.log(this.player);
+	this.nextButton = document.getElementById('nextsong');
+	this.nextButton.addEventListener('click', this.onNextClick, false);
+
+	this.progressBar = document.getElementsByClassName('bar')[0];
+	this.progressBar.style.width = '0%';
+
+	this.volume = document.getElementById('volume');
+	this.volume.addEventListener('click', this.onMuteClick, false);
+
+	jQuery('#playprogress > div').on('click', this.onProgressClick);
+
+	this.player.ontimeupdate = this.onProgress;
+	this.player.onended = this.onNextClick;
+
+	this.songLinks = jQuery('#songlist u');
+	jQuery(this.songLinks).html(this.createSongList());
+	this.setUpSongList();
+
+	this.loadNewSong();
 };
